b7048fe9072b9eb20e8e7521ab23f353482af4e8
ionutiacob33 <ionut.iacobmare@gmail.com>
2022-03-11 09:54:57 +0200

    Added basic user signup

--- src/main/java/mealsapp/service/AuthService.java
+++ src/main/java/mealsapp/service/AuthService.java
@@ -1,9 +1,37 @@
 package mealsapp.service;
 
 import lombok.AllArgsConstructor;
+import mealsapp.controller.AuthController;
+import mealsapp.dto.RegisterRequest;
+import mealsapp.model.User;
+import mealsapp.repository.UserRepository;
+import mealsapp.repository.VerificationTokenRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
 
 @Service
 @AllArgsConstructor
 public class AuthService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final VerificationTokenRepository verificationTokenRepository;
+
+    @Transactional
+    public void signup(RegisterRequest registerRequest) {
+        User user = new User();
+        user.setUsername(registerRequest.getUsername());
+        user.setEmail(registerRequest.getEmail());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        user.setCreated(Instant.now());
+        user.setEnabled(false);
+
+        userRepository.save(user);
+
+        // TODO: Generate verification token and send it by email
+    }
+
 }
