fde84bcd2541dabe57cac76603a3c6be5c3d108b
ionutiacob33 <ionut.iacobmare@gmail.com>
2022-04-18 10:15:17 +0300

    Added refresh token functionality

--- src/main/java/mealsapp/controller/AuthController.java
+++ src/main/java/mealsapp/controller/AuthController.java
@@ -1,7 +1,11 @@
 package mealsapp.controller;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
-import mealsapp.dto.LoginRequest;
 import mealsapp.dto.RegisterRequest;
 import mealsapp.model.Response;
 import mealsapp.model.User;
@@ -9,10 +13,20 @@ import mealsapp.service.AuthService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 import static java.time.LocalDateTime.now;
+import static mealsapp.utils.Constants.JWT_EXPIRY_MILLIS;
+import static mealsapp.utils.Constants.JWT_SECRET;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
 import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -47,6 +61,11 @@ public class AuthController {
                         .statusCode(OK.value())
                         .build()
         );
+    }
+
+    @GetMapping("/token/refresh")
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        authService.refreshToken(request, response);
     }
 
 }
